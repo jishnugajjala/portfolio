110944f628e1431fb072fb28ca8a39d761e5dbdc
Gajjala Jishnu <jishnu0703@gmail.com>
2026-01-10 07:23:46 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactSection = document.getElementById("contact");
+
+    contactSection.addEventListener("click", function () {
+        alert("Thanks for visiting my portfolio! Feel free to reach out.");
+    });
+});
